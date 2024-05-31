b24f2993b3ec3f75a5be06a73efb80d1ebb7df43
hounfodji <hospicehounfodji@gmail.com>
2024-05-31 19:19:22 +0100

    fluuteer_chat_app - add onboarding

--- flutter_chat_app/lib/main.dart
+++ flutter_chat_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_chat_app/MyHomePage.dart';
+import 'package:flutter_chat_app/onboarding_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const OnBoardingPage(),
     );
   }
 }
---
+++ flutter_chat_app/lib/onboarding_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/my_home_page.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  const OnBoardingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Text(
+                  'You AI Assistant',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue
+                  ),
+                ),
+                SizedBox(height: 16,),
+                Text(
+                  'Using this software, you can ask you questions and receive articles using artificial intelligence assistant',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black54
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 32,),
+            Image.asset('assets/images/onboarding.png'),
+            SizedBox(height: 32,),
+            ElevatedButton(
+                onPressed: (){
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => const MyHomePage()),
+                          (route) => false
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32)
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text('Continue'),
+                    SizedBox(height: 8,),
+                    Icon(Icons.arrow_forward)
+                  ],
+                )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

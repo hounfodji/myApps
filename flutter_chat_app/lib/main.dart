27f049e023a0b703fc9a9c925deda01cd842498b
hounfodji <hospicehounfodji@gmail.com>
2024-05-31 20:19:14 +0100

    flutter_chat_app - use themeData with riverpod

--- flutter_chat_app/lib/main.dart
+++ flutter_chat_app/lib/main.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/onboarding_page.dart';
+import 'package:flutter_chat_app/theme_notifier.dart';
 import 'package:flutter_chat_app/themes.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+      ProviderScope(child: MyApp())
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeProvider);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: lightMode,
       darkTheme: darkMode,
+      themeMode: themeMode,
       home: const OnBoardingPage(),
     );
   }
